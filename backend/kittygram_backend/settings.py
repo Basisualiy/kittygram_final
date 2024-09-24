61be023bd63f76101b203cd1263f837e4aef2243
Vasiliy Yakovlev <Basisualiy01@yandex.ru>
2024-09-24 11:46:32 +0300

    fixed static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -97,6 +97,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/backend_static'
+STATICFILES_DIRS = ['/static/backend_static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
